4b44e5e05bb5898069847e77fdb8a651b445de12
Oli Elliott <oe3257@gmail.com>
2019-01-09 20:33:50 +0000

    read raw world bank indicator csvs

--- R/read_raw_data.R
+++ R/read_raw_data.R
@@ -8,3 +8,11 @@ read_ipu_csv <- function(file, data_name){
                   skip = 3)
 }
 
+read_world_bank_indicator_csv <- function(file){
+  #'  Read the population percentage of women raw file
+  #'  
+  
+  readr::read_csv(file,
+                  col_names = TRUE,
+                  skip = 4)
+}
