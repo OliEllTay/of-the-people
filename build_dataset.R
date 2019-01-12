d835f957ea7574cdf115ba7e2ca502a673d9c641
Oli Elliott <oe3257@gmail.com>
2019-01-12 15:18:27 +0000

    data set needs to be in the processing directory

--- build_dataset.R
+++ build_dataset.R
@@ -3,7 +3,7 @@ build_dataset <- function(){
   
   source(here::here("R","viz_data.R"), local = TRUE)
   
-  data_location <- here::here("data","viz_data.csv")
+  data_location <- here::here("of_the_people_viz","viz_data.csv")
   viz_data() %>%
     readr::write_csv(data_location)
 }
